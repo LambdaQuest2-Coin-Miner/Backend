a6391f6cfc2405879fbc017bd5a017e28a3e59f3
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-02-28 10:26:00 +0800

    Add CORS

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from functools import wraps
 from dotenv import load_dotenv
 import os
 
 # create the application object
 app = Flask(__name__)
+
+# enable CORS
+CORS(app)
 
 # config
 load_dotenv()
@@ -15,12 +19,30 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 
 # import db schema
-from models import *
+from models import Room, Player
 
 # routes
 @app.route('/')
 def home():
     return "Welcome to LambdaQuest2: Coin Miner"
+
+
+@app.route('/api')
+def api_status():
+    response = {"status": "up"}
+    return jsonify(response), 200
+
+
+@app.route('/api/rooms')
+def rooms_api():
+    if request.method == 'GET':
+        try:
+            response = db.session.query(Room).all()
+            print(response)
+        except:
+            flash('Missing the DB!')
+        return "Getting rooms"
+
 
 if __name__ == '__main__':
     app.run()
