dd010b01a1faa5c989b4b60b9a5f99d190bbc33a
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-02-28 17:06:15 +0800

    Improve code readability

--- cool_down_util.py
+++ cool_down_util.py
@@ -1,22 +1,4 @@
-def parse_cooldown_message(messages):
-    total = 0
-
-    if messages is not None or len(messages) > 0:
-        is_cooldown = [
-            cooldown_str for cooldown_str in messages if "CD" in cooldown_str]
-
-        if len(is_cooldown) > 0:
-            message_cooldown = [int(char)
-                                for char in is_cooldown[0] if char.isdigit()][0]
-            print(f"Cooldown points: {message_cooldown}")
-            total += message_cooldown
-
-        else:
-            print(f"No cooldown points")
-
-    return total
-
-
+# Dynamically calculate cooldown points based on player and room factors
 def cooldown_calc(player_cd, room_cd, errors, messages=[]):
     total_cooldown = float(player_cd) + float(room_cd)
     if len(errors) > 0:
@@ -26,3 +8,23 @@ def cooldown_calc(player_cd, room_cd, errors, messages=[]):
 
     print(f"Total cooldown points: {total_cooldown}")
     return total_cooldown
+
+
+# Extract cooldown numbers from errors and messages
+def parse_cooldown_message(messages):
+    total = 0
+
+    if messages is not None or len(messages) > 0:
+        is_cooldown = [
+            cooldown_str for cooldown_str in messages if "CD" in cooldown_str]
+
+        if len(is_cooldown) > 0:
+            extract_cooldown_pts = [int(char)
+                                    for char in is_cooldown[0] if char.isdigit()][0]
+            print(f"Cooldown points extracted: {extract_cooldown_pts}")
+            total += extract_cooldown_pts
+
+        else:
+            print(f"No cooldown points")
+
+    return total
