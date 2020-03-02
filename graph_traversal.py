096e5a427efcdfa57424ce8298ec56de0ee7e698
Jamar Torres <Jetorres88@gmail.com>
2020-03-01 20:48:29 -0500

    Fixed errosr to run without breaking.
    Co-authored-by: claridiva2000 webdevjaz@gmail.com

--- graph_traversal.py
+++ graph_traversal.py
@@ -6,6 +6,7 @@ import socket
 from config import API_TOKEN
 #from player import Player
 from cool_down_util import cooldown_calc
+from util import Queue, Stack
 
 #api endpoints
 BASE_URL = "https://lambda-treasure-hunt.herokuapp.com/api/adv/"
@@ -22,11 +23,10 @@ STATUS_URL = BASE_URL + "status/"
 
 headers = {'Authorization': f'Token {API_TOKEN}'}
 
-from util import Queue, Stack
 
 r = requests.get(f"{BASE_URL}/init/", headers=headers).json()
 
-reverese_directions = {'n':'s', 's':'n','e':'w','w':'e'}
+reverse_directions = {'n':'s','s':'n','e':'w','w':'e'}
 
 reverse_path = []
 visited = {}
@@ -36,72 +36,73 @@ traversal_path = []
 def get_player_status():
 	req = requests.post(url=STATUS_URL, headers=headers)
 	data = req.json()
-	print('player_data', data)
+	print('player_data status:', data)
 	return data
+
 
 # Send request to init to get the current room information
 def get_room_info():
-	req = requests.get(url = INIT_URL, headers=headers)
+	req = requests.get(url=INIT_URL, headers=headers)
 	# getting data in the json format
 	data = req.json()
 	print('data from room info', data)
 	return data
 
 # randomizer is here from a Graphs project as an alternative way to traverse the graph
-def randomizer(room_id, doors):
-	req = requests.get(url = INIT_URL, headers=headers)
-	# getting data in the json format
-	data = req.json()
-	print('data from room info', data)
+# def randomizer(room_id, doors):
+# 	req = requests.get(url = INIT_URL, headers=headers)
+# 	# getting data in the json format
+# 	data = req.json()
+# 	print('data from room info', data)
 	
 	
-	# data = get_room_info()
-	if len(doors) > 1:
+# 	# data = get_room_info()
+# 	if len(doors) > 1:
 		
-		rando = random.randint(1,len(doors)-1)
-		print(f"from randomizer {room_id} {doors[rando]}")
-		return doors[rando]
-	else:
-		doors = data
-		print('data in randomizer', doors)
-		return doors
-	# return data
+# 		rando = random.randint(1,len(doors)-1)
+# 		print(f"from randomizer {room_id} {doors[rando]}")
+# 		return doors[rando]
+# 	else:
+# 		doors = data
+# 		print('data in randomizer', doors)
+# 		return doors
+# 	# return data
 
 
 def find_next_dir(current_room, visited):
-    #  picks a random unexplored direction from the player's current room's exits list, 
-    # If no available exits, return None
+	# Picks a random unexplored direction from available rooms exit list; returns None if no exits avail
 
-    # get allowed exits from the current_room
-    travel_dir = None
-    cooldown = 0
-    if current_room not in visited:
-        exits = r['exits']
-        coordinates = r['coordinates']
+	# get allowed exits from the current_room
+	travel_dir = None
+	cooldown = 0
+	if current_room not in visited:
+		r = get_room_info()
+		exits = r['exits']
+		coordinates = r['coordinates']
 
-        cooldown = r['cooldown']
-        title = r['title']
-        # Wait for cooldown
-        time.sleep(cooldown)
-        directions = {}
-        for direction in exits:
-            directions[direction] = '?'
-        info = {}
-        info['directions'] = directions
-        info["coordinates"] = coordinates
-        info['title'] = title
-        visited[current_room] = info
-        travel_dir = random.choice(list(directions.keys()))
-    else:
-        # check if there's an exit that is not ?
-        directions = visited[current_room]['directions']
-        possible_dirs = []
-        for direction in directions:
-            if directions[direction] == '?':
-                possible_dirs.append(direction)
-        if len(possible_dirs) != 0:
-            travel_dir = random.choice(possible_dirs)
-    return (travel_dir, cooldown)
+		cooldown = r['cooldown']
+		title = r['title']
+		# Wait for cooldown
+		time.sleep(cooldown)
+		directions = {}
+		for direction in exits:
+			directions[direction] = '?'
+		info = {}
+		info['directions'] = directions
+		info["coordinates"] = coordinates
+		info['title'] = title
+		visited[current_room] = info
+		travel_dir = random.choice(list(directions.keys()))
+	else:
+		# check if there's an exit that is not ?
+		directions = visited[current_room]['directions']
+		possible_dirs = []
+		for direction in directions:
+			if directions[direction] == '?':
+				possible_dirs.append(direction)
+		if len(possible_dirs) != 0:
+			travel_dir = random.choice(possible_dirs)
+	return (travel_dir, cooldown)
 
 # create movement function  
 		#set direction/movement via a variable through the object
@@ -110,26 +111,28 @@ def move_next_direction(direction, visited):
 	# maintain tracking information 
 	# use post request with authorization token 
 	
-	DIRECTIONS = {'direction': direction}
+	DIRECTIONS = {"direction": direction}
 	
-	print(f"room room: {r['room_id']}")
-	# cooldown = info['cooldown']
+	data = get_room_info()
+	room = data['room_id']
+	
+	cooldown = data['cooldown']
 	# timeout = socket.settimeout()
 	# timeout(cooldown)
-	#req = requests.post(url = MOVE_URL, json={"exit":"n"}, headers=headers)
-	req = requests.post(url = MOVE_URL, json=DIRECTIONS, headers=headers)
-	data = req.json()
-	print('data post move next direction', data)
-	room_id = r['room_id']
-	cooldown = r['cooldown']
-	print('room_id', room_id)
 	time.sleep(cooldown)
-	if room_id in visited and visited[room_id]['directions'][direction] != '?':
-		next = visited[room_id]['directions'][direction]
-		DIRECTIONS = {'next_room_id': next}
+	#req = requests.post(url = MOVE_URL, json={"exit":"n"}, headers=headers)
+	#req = requests.post(url = MOVE_URL, json=DIRECTIONS, headers=headers)
+	# info = req.json()
+	if room in visited and visited[room]['directions'][direction] != '?':
+		next = visited[room]['directions'][direction]
+		DIRECTIONS['next_room_id'] = f"{next}"
+		#DIRECTIONS = {'next_room_id': next}
 	req = requests.post(url = MOVE_URL, json=DIRECTIONS, headers=headers)
+	#print('data info post move next direction', data)
 	# Calculate cooldown points
+	data = req.json()
 	player_status = get_player_status()
+	#print(f"room room: {r['room_id']}")
 	print(f"player cooldown points: {player_status['cooldown']}")
 	try:
 		cooldown_points = cooldown_calc(player_status['cooldown'], data['cooldown'], data['errors'], data['messages'])
@@ -138,23 +141,28 @@ def move_next_direction(direction, visited):
 		cooldown_points = cooldown_calc(player_status['cooldown'], data['cooldown'], data['errors'])
 
 	print('cooldown points: ', cooldown_points)
-	#print('move data room and cool', (data['room_id', data['cooldown']]))
-	#return (data['room_id', data['cooldown']])
-	return data
+	print('move data room and cool', (data['room_id'], data['cooldown']))
+	return (data['room_id'], data['cooldown'])
+	# return data
+
+
 	
 def find_next_path(visited):
-	room = r['room_id']
-	cooldown = r['cooldown']
+	data = get_room_info()
+	room = data['room_id']
+	cooldown = data['cooldown']
+	# room = r['room_id']
+	# cooldown = r['cooldown']
 	time.sleep(cooldown)
-	queue = Queue()
+	q = Queue()
 	traveled_rooms = set()
 	# queue.enqueue([(None, room_id)])
 	# stack a list for next path
 	# queue.enqueue([room])
-	queue.enqueue([(None, room)])
+	q.enqueue([(None, room)])
 
-	while queue.size() > 0:
-		current_path = queue.dequeue()
+	while q.size() > 0:
+		current_path = q.dequeue()
 		# possibly should be room_node = current_path[-1][1] ?
 		room_node = current_path[-1][1]
 		direction, cooldown = find_next_dir(room_node, visited)
@@ -171,8 +179,8 @@ def find_next_path(visited):
 			exits = visited[room_node]['directions']
 			for exit in exits:
 				path_copy = list(current_path)
-				path_copy.append(exit, exits[exit])
-				queue.enqueue(path_copy)
+				path_copy.append((exit, exits[exit]))
+				q.enqueue(path_copy)
 				print('enqueued', path_copy)
 			
 			# for exit in exits[exit]["exits"].key():
@@ -190,13 +198,16 @@ def find_next_path(visited):
 	return None
 
 def traversal_path_end(visited):
+	# r = requests.get(f"{BASE_URL}/init/", headers=headers).json()
+	# print(r)
+	# print(f"Starting room: {r['cooldown']}")
 	data = get_room_info()
-	cooldown = data['cooldown']
-	print('rrrrrrr', r)
-	# connecting to room_id (having trouble accessing init object (point is to get the room_id but for some reason it does not in some cases))
+	print('data info traversal', data)
 	room = r['room_id']
-	# invoking cooldown 
+	print('room in traversal', room)
+	cooldown = data['cooldown']
 	time.sleep(cooldown)
+	# connecting to room_id (having trouble accessing init object point is to get the room_id but for some reason it does not in some cases))
 	# Point of this is to try to find the next room and cooldown connects to finding the next current room that has not been explored yet. 
 	next_room, cooldown = find_next_dir(room, visited)
 	# waiting for cooldown here.. 
@@ -204,7 +215,10 @@ def traversal_path_end(visited):
 	# while the next direction is not None.. 
 	while next_room is not None:
 		## set the new room and cooldown to match the players next direction with the current room and visited called 
+		
 		room_explored, cooldown = move_next_direction(next_room, visited)
+		#print('room_explored ', room_explored)
+		#print('and cooldown', cooldown)
 		# cooldown.. am I using cool down too much?
 		time.sleep(cooldown)
 
@@ -212,18 +226,24 @@ def traversal_path_end(visited):
 		visited[room]['directions'][next_room] = room_explored
 		# here is to save the current room in the new room's direction
 		if room_explored not in visited:
+			data = get_room_info()
+			print('data in room_explored not in visited', data)
 			# room_info = get_room_info()
 			# room_info = r()
 			# print('room_info', r())
 			## this is to print out the information needed from the init function 
+			# exits = data['exits']
+			# coordinates = data['coordinates']
+			# title = data['title']
 			exits = r['exits']
 			coordinates = r['coordinates']
-			cooldown = r['cooldown']
 			title = r['title']
+			cooldown = data['cooldown']
+			
 			time.sleep(cooldown)
 			directions_to_visit = {}
 			for direction in exits:
-				if direction == reverese_directions[next_room]:
+				if direction == reverse_directions[next_room]:
 					directions_to_visit[direction] = room
 				else:
 					directions_to_visit[direction] = '?'
@@ -234,14 +254,14 @@ def traversal_path_end(visited):
 			room_information['title'] = title
 			visited[room_explored] = room_information
 		else:
-			visited[room_explored]['directions'][reverese_directions[next_room]] = room
+			visited[room_explored]['directions'][reverse_directions[next_room]] = room
 
 		next_room, cooldown = find_next_dir(room_explored, visited)
-
 		time.sleep(cooldown)
+		data = get_room_info()
 
 		room = r['room_id']
-		cooldown = r['cooldown']
+		cooldown = data['cooldown']
 		time.sleep(cooldown)
 		
 
@@ -249,13 +269,13 @@ def traversal_path_end(visited):
 
 # find_next_path()
 
-
-room_graph = get_room_info()
+# room_graph = get_room_info()
 
 travel_through_map = True
 
 # intention is to continously look through new rooms with while loop until all are visited.. 
 while travel_through_map is True:
+	print('visited: ', visited)
 	# invoking traversal to end paths
 	traversal_path_end(visited)
 	#randomizer(r['room_id'], visited)
@@ -277,4 +297,4 @@ while travel_through_map is True:
 # Final graph - after visiting all rooms
 print(f"GRAPH: {visited}")
 with open('graph.json', 'w') as fp:
-    json.dump(visited, fp)
+	json.dump(visited, fp)
